2e35cc01d214948801c9cba7540c6ad15a401018
Farid Aguilar <faridaguilar@hotmail.com>
2023-12-14 19:05:03 -0600

    Agregamos funcionalidad del proyecto faltando CSS, un poco de HTML y algunas comprobaciones

---
+++ main.js
@@ -0,0 +1,60 @@
+const usuarios = {
+        usuario1: { contrasena: 'contrasena1', saldo: 1000 },
+        usuario2: { contrasena: 'contrasena2', saldo: 1500 },
+        usuario3: { contrasena: 'contrasena3', saldo: 2000 }
+    };
+
+    let usuarioActual = null;
+
+    function iniciarSesion() {
+        const usuarioInput = document.getElementById('usuario').value;
+        const contrasenaInput = document.getElementById('contrasena').value;
+
+        if (usuarios[usuarioInput] && usuarios[usuarioInput].contrasena === contrasenaInput) {
+            usuarioActual = usuarioInput;
+            mostrarResultado(`Bienvenido, ${usuarioActual}.`);
+        } else {
+            mostrarResultado('Credenciales incorrectas. Inténtelo de nuevo.');
+        }
+    }
+
+    function consultarSaldo() {
+        if (usuarioActual) {
+            const saldo = usuarios[usuarioActual].saldo;
+            mostrarResultado(`Saldo disponible: $${saldo}`);
+        } else {
+            mostrarResultado('Inicie sesión para consultar saldo.');
+        }
+    }
+
+    function ingresarMonto() {
+        if (usuarioActual) {
+            const monto = parseFloat(prompt('Ingrese el monto a depositar:'));
+            if (!isNaN(monto) && monto > 0) {
+                usuarios[usuarioActual].saldo += monto;
+                mostrarResultado(`Se ha ingresado $${monto} a su cuenta.`);
+            } else {
+                mostrarResultado('Ingrese un monto válido.');
+            }
+        } else {
+            mostrarResultado('Inicie sesión para ingresar monto.');
+        }
+    }
+
+    function retirarMonto() {
+        if (usuarioActual) {
+            const monto = parseFloat(prompt('Ingrese el monto a retirar:'));
+            if (!isNaN(monto) && monto > 0 && monto <= usuarios[usuarioActual].saldo) {
+                usuarios[usuarioActual].saldo -= monto;
+                mostrarResultado(`Se ha retirado $${monto} de su cuenta.`);
+            } else {
+                mostrarResultado('Monto no válido o insuficiente saldo.');
+            }
+        } else {
+            mostrarResultado('Inicie sesión para retirar monto.');
+        }
+    }
+
+    function mostrarResultado(mensaje) {
+        document.getElementById('resultado').innerText = mensaje;
+    }
